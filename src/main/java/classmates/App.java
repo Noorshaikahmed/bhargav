ee9cbb333c67733cacff5bef8eee78879265070d
EC2 Default User <ec2-user@ip-172-31-8-38.ap-south-1.compute.internal>
2022-04-03 08:06:17 +0000

    hiii dear

---
+++ src/main/java/classmates/App.java
@@ -0,0 +1,13 @@
+package classmates;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
